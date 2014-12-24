1de9559f0c60d941a6ffd0717baf6a790b966825
bambreeze <bambreeze@gmail.com>
2014-12-25 06:33:46 +0800

    at24c02 - fix bug & clean codes

--- at24c02/at24c02.c
+++ at24c02/at24c02.c
@@ -10,31 +10,30 @@ int main(int argc, char **argv)
 {
     int rc = 0;
     int i = 0;
-    unsigned char len = 0;
-    unsigned char rbuf[16];
-    unsigned char wbuf[16];
+    int counter = 0;
+    unsigned char rbuf = 0;
 
 	if (!bcm2835_init())  
 		return 1;  
 
     bcm2835_i2c_begin();
     bcm2835_i2c_setSlaveAddress(I2C_ADDR_AT24C02);
-    //bcm2835_i2c_setClockDivider(/*BCM2835_I2C_CLOCK_DIVIDER_2500*/7500); /* ~32 kHz */
+    bcm2835_i2c_set_baudrate(10000); /* 10 KHz */
     
     // read
-    len = 1;
-    rc = bcm2835_i2c_read(rbuf, len);
-    printf("Read Result = %d\n", rc);
-    for (i = 0; i < len; i++) {
-        printf("Read rbuf[%d] = %x\n", i, rbuf[i]);
+    counter = 8;
+    for (i = 0; i < counter; i++) {
+        rc = bcm2835_i2c_read(&rbuf, 1);
+        if (!rc) {
+            printf("Read rbuf = %x\n", rbuf);
+        } else {
+            printf("ERROR! rc = %d\n", rc);
+        }
     }
 
-    // write
-    /*wbuf[0] = 0x5a;
-    rc = bcm2835_i2c_write(wbuf, len);
-    printf("Write Result = %d\n", rc);*/
-
-    bcm2835_i2c_end();   
+    // Just leave them as i2c pin to make i2c tools happy :-)
+    //bcm2835_i2c_end();   
     bcm2835_close();
+
     return 0;
 }
