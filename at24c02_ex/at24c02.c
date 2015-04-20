499f91afcfa4c414c0ff1760b7f471b287ecd6c5
bambreeze <bambreeze@gmail.com>
2015-04-20 22:31:59 +0800

    update at24c02 write driver, still fail

--- at24c02_ex/at24c02.c
+++ at24c02_ex/at24c02.c
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
+#include "i2c-dev.h"
 
 #define AT24C02_I2C_ADDR 0x50  
 
@@ -15,18 +14,21 @@ int i2c_write_data (int fd, uint16_t addr, uint8_t offset, uint8_t val)
 
     data = (struct i2c_rdwr_ioctl_data *) malloc(sizeof(struct i2c_rdwr_ioctl_data));
     if (!data) {
+        printf("Memory alloc failed for *struct i2c_rdwr_ioctl_data*\n");
         return -1;
     }
 
     data->nmsgs = 1;
     data->msgs  = (struct i2c_msg *) malloc(data->nmsgs * sizeof(struct i2c_msg));
     if (!data->msgs) {
+        printf("Memory alloc failed for *struct i2c_msg*\n");
         free(data);
         return -1;
     }
 
     data->msgs[0].buf = (unsigned char *) malloc(2 * sizeof(unsigned char));
     if (!data->msgs[0].buf) {
+        printf("Memory alloc failed for *msg buf*\n");
         free(data->msgs);
         free(data);
         return -1;
@@ -39,6 +41,7 @@ int i2c_write_data (int fd, uint16_t addr, uint8_t offset, uint8_t val)
     data->msgs[0].buf[1] = val;
 
     if (ioctl(fd, I2C_RDWR, (unsigned long)data) < 0) {
+        printf("ioctl failed\n");
         free(data->msgs[0].buf);
         free(data->msgs);
         free(data);
@@ -80,7 +83,13 @@ int main (void)
         }
     }
 
-    // write
+    // Random read
+    data[0] = i2c_smbus_read_byte_data(fd, 0x3);
+    printf("The 4th data is: %x\n", data[0]);
+
+    data[0] = i2c_smbus_read_byte_data(fd, 0x1);
+    printf("The 2nd data is: %x\n", data[0]);
+
     data[0] = 'H';
     data[1] = 'e';
     data[2] = 'l';
@@ -95,8 +104,17 @@ int main (void)
     data[11] = 'd';
     data[12] = '!';
 
+    // write using raw i2c commands - failed
     for (i = 0; i < 16; i++) {
         if (i2c_write_data (fd, AT24C02_I2C_ADDR, i, data[i])) {
+            printf("write fail!\n");
+            return -1;
+        }
+    }
+
+    // write using smbus commands - failed
+    for (i = 0; i < 16; i++) {
+        if (i2c_smbus_write_byte_data(fd, i, data[i])) {
             printf("write fail!\n");
             return -1;
         }
